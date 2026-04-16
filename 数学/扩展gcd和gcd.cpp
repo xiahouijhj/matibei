70b06505b54caec617d9023b186374dbf46bdac4
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2026-04-16 14:05:20 +0800

    Update 扩展gcd和gcd.cpp

--- 数学/扩展gcd和gcd.cpp
+++ 数学/扩展gcd和gcd.cpp
@@ -1,1 +1,25 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+/**
+ * @param a, b 输入的两个数
+ * @param x, y 引用传递，用于返回 ax + by = gcd(a, b) 的一组解
+ * @return 返回 a 和 b 的最大公约数
+ */
+long long exgcd(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1; 
+        y = 0;
+        return a;
+    }
+    long long d = exgcd(b, a % b, y, x); // 注意这里 y 和 x 的位置交换
+    y -= (a / b) * x;
+    return d;
+}
+//gcd求最大公约数
+long long gcd(long long a, long long b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
