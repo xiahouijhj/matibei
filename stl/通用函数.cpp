79470046058f83f557fcbf0976b82ed7ad312a2f
xiahouijhj <120567551+xiahouijhj@users.noreply.github.com>
2026-04-17 10:08:47 +0800

    Update 通用函数.cpp

--- stl/通用函数.cpp
+++ stl/通用函数.cpp
@@ -23,4 +23,15 @@ int main()
     {
         cout<<"a不为空集"<<endl;
     }
+    int target=2;
+    // 返回第一个 >=2  的迭代器
+    auto lb = lower_bound(a.begin(), a.end(), target);
+    // 返回第一个 >2 的迭代器
+    auto ub = upper_bound(a.begin(), a.end(), target);
+    //注意：set和multiset还有map需要用内置lower_bound和upper_bound,例子如下
+    set<int> s={1,2,3,4};
+    // 返回第一个 >=2  的迭代器
+    auto sa = s.lower_bound(target);
+    // 返回第一个 >2 的迭代器
+    auto sb = s.upper_bound(target);
 }
